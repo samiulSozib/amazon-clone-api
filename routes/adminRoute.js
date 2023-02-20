459692354c66062473d3fa3bff124fa2e69452da
Md Samiul <samiuljust2018@gmail.com>
2023-02-20 21:19:32 +0600

    update

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const { addProduct } = require('../controllers/adminController')
 const admin = require('../middlewares/adminMiddleware')
 
-router.post('/add-product', admin, addProduct)
+router.post('/add-product', addProduct)
 
 module.exports = router
